70905b0f8065fe9c7701c26e53ec5ced64cf6bf7
Lara Cunha <lara2003cunha@gmail.com>
2022-12-18 14:53:31 +0000

    Create class StringDrink

--- src/main/java/com/liac1983/patterns-master/Main.java
+++
@@ -1,7 +0,0 @@
-package com.liac1983.patterns-master;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/transformer/StringTransformer.java
+++
@@ -1,7 +0,0 @@
-package transformer;
-
-public interface StringTransformer {
-    void execute(StringDrink drink);
-
-    void undo(StringDrink drink);
-}
