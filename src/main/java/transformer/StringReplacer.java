fb8d2c4f7bb09923a44d2aa9efd91fe2f2948572
Lara Cunha <lara2003cunha@gmail.com>
2022-12-18 12:06:41 +0000

    Create StringReplacer

---
+++ src/main/java/transformer/StringReplacer.java
@@ -0,0 +1,26 @@
+package transformer;
+
+public class StringReplacer implements StringTransformer {
+    private char character;
+    private char replacement;
+
+    public StringReplacer(char character, char replacement) {
+        this.character = character;
+        this.replacement = replacement;
+    }
+
+
+    @Override
+    public void execute(StringDrink drink) {
+        String replaced = drink.getText()
+                .replace(character, replacement);
+        drink.setText(replaced);
+    }
+
+    @Override
+    public void undo(StringDrink drink) {
+        String replaced = drink.getText()
+                .replace(replacement, character);
+        drink.setText(replaced);
+    }
+}
